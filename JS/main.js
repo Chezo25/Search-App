e343b05ce1fc0481932c1b1fac423dfdc888911c
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-30 12:51:23 -0400

    add more style
    more style code

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("readystatechange", (evt) => {
+    if (event.target.readyState === "complete") {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // set the focus
+
+    // 3 listeners clear text
+
+    const form = document.getElementById("searchBar");
+    form.addEventListener("submit",
+        submitTheSearch);
+}
