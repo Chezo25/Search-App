6f8d9c1684607faf245d7d01a08ad19b8582aabb
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-31 18:39:30 -0400

    Update searchResults.js
    -add more code for images

--- JS/searchResults.js
+++ JS/searchResults.js
@@ -10,6 +10,29 @@ export const buildSearchResults = (resultArray) => {
         const resultText = createResultText(result);
         resultContents.append(resultText);
         resultItem.append(resultContents);
-        const searchResults
-    })
+        const searchResults = document.getElementsById("searchResults");
+        searchResults.append(resultItem);
+    });
+}
+
+const createResultItem = (result) => {
+    const resultItem = document.createElement("div");
+    resultItem.classList.add("resultItem");
+    const resultTitle = document.createElement("div");
+    resultTitle.classList.add("resultTitle");
+    const link = document.createElement("a");
+    link.href = `https: //en.wikipedia.org/?curid=${result.id}`;
+    link.textContent = result.title;
+    link.target = "_blank";
+    resultTitle.append(link);
+    resultItem.append(resultTitle);
+    return resultItem;
+}
+
+const createResultImage = (result) => {
+    const resultImage = document.createElement("div");
+    resultImage.classList.add("resultImage");
+    const img = document.createElement("img");
+    img.src = result.img;
+    img.alt = resultImage
 }
