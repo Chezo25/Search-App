3ea381d2c531062876d0fb418c9e60c63de51282
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2020-12-30 22:40:57 -0400

    Update dataFunctions.js
    add array function

--- JS/dataFunctions.js
+++ JS/dataFunctions.js
@@ -7,11 +7,17 @@ export const getSearchTerm = () => {
 
 export const retrieveSearchResults = async(searchTerm) => {
     const wikiSearchString = getWikiSearchString(searchTerm);
+    const wikiSearchResults = await requestData(wikiSearchString);
+    let resultArray = [];
 }
 
 const getWikiSearchString = (searchTerm) => {
     const maxChars = getMaxChars();
-}
+    const rawSearchString = `https://en.wikipedia.org/w/api.php?action=query&generator=search&meta=
+    siteinfo&exintro&siprop=namespaces&format=json&origin=*`;
+    const searchString = encodeURI(rawSearchString);
+    return searchString;
+};
 
 const getMaxChars = () => {
     const width = window.innerWidth || document.body.clientWidth;
@@ -20,4 +26,14 @@ const getMaxChars = () => {
     if (width >= 414 && width < 1400) maxChars = 102;
     if (width >= 1400) maxChars = 130;
     return maxChars;
+};
+
+const requestData = async(searchString) => {
+    try {
+        const response = await fetch(searchString);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
 };
